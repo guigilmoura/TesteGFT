347da7cc0915f4080bd2f804f9901d17fb06a438
guigilmoura <gui.gil.moura@gmail.com>
2022-06-28 18:17:11 -0300

    coloquei os trabalhadores num vetor. este seria o passo para criar as possíveis consultas de atributos

--- Teste/teste-tecnico-gft/src/testeTecnico/Questao03.java
+++ Teste/teste-tecnico-gft/src/testeTecnico/Questao03.java
@@ -13,6 +13,7 @@ public class Questao03 {
             qtdeTrabalhadores = scan.nextInt();
             if (qtdeTrabalhadores > 4) x = false;
         }
+        Trabalhador[] trabalhadores = new Trabalhador[qtdeTrabalhadores];
         for (int i = 1;i <= qtdeTrabalhadores; i++) {
             System.out.println("entre o nome do trabalhador "+ indice);
             String nome = scan.next();
@@ -22,6 +23,7 @@ public class Questao03 {
             int horasTrabalhadas = scan.nextInt();
             Trabalhador trabalhador = new Trabalhador(nome, salarioHora, horasTrabalhadas );
             indice ++;
+            trabalhadores[i] = trabalhador;
         }
         //public static void consultar(String info){
 
